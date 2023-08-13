d6cecbcdef2996869b62551e15eb8d8323840e79
Masud <ranamasud0701@gmail.com>
2023-08-14 00:24:33 +0600

    first commit

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,11 @@
+
+
+const Main = () => {
+    return (
+        <div>
+            <h1 className="text-center text-lg text-red-600">This is masud</h1>
+        </div>
+    );
+};
+
+export default Main;
